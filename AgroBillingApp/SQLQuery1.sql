dc387afe1ca950d49f40baa589e41a6ba808beac
Mukesh Kumar <sihagmukesh9977@gmail.com>
2026-04-08 00:19:46 +0530

    new correction

--- AgroBillingApp/SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-﻿INSERT INTO AdminUsers (FullName, Email, PasswordHash, IsActive, CreatedAt)
-VALUES (
-  'Mukesh Admin',
-  'admin@agrobilling.com',
-  LOWER(CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', 'admin123'), 2)),
-  1,
-  GETDATE()
-);
